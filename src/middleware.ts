12a4718ced075eb11790953704a411f62289a105
rrhan32 <96813144+rrhan32@users.noreply.github.com>
2024-02-16 02:18:17 +0530

    added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+
+import { NextResponse , NextRequest } from "next/server";
+
+export function middleware(request:NextRequest){
+    const path=request.nextUrl.pathname;
+    const isPublicPath= path==='/login'||path==='/signUp';
+    const token = request.cookies.get('token')||"";
+
+    if (isPublicPath && token){
+        return NextResponse.redirect(new URL('/',request.nextUrl))
+    }    // if your are logged in , then you not see the login and signup page
+    
+    if (!isPublicPath && !token){
+        return NextResponse.redirect(new URL("/login",request.nextUrl));  //if you try to access a private route 
+      //if it is private route but user did not logedin,then redirect to
+
+    }
+
+}
+
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: [
+      '/',
+      '/profile',
+      '/login',
+      '/signUp',
+      '/verifyemail'
+    ]
+  }
