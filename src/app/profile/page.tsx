b3d9d91d44620d3a46d1ebea2d5c96a15cda1a05
rrhan32 <96813144+rrhan32@users.noreply.github.com>
2024-02-16 02:18:49 +0530

    modified profile page to show user id

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,54 @@
-"use client"
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import axios from "axios";
+import { toast, Toaster } from "react-hot-toast";
 
 function Profile() {
-    return(
-      <div className="flex flex-col text-center justify-center min-h-screen text-red-600">Toh kaise hai aaplog</div>
-    )
+  const router = useRouter();
+  const SearchParams = useSearchParams();
+  const [loginSuccess, setLoginSuccess] = useState(SearchParams.get("loginSuccess")!);
+  const [data, setData] = useState("");
+  useEffect(() => {
+    const noti=localStorage.getItem('notifshown');
+    console.log(noti);
+    if (loginSuccess === "true" && noti=== null) {
+      // Show success toast
+      console.log("reached here")
+      toast.success("Login successful");
+      localStorage.setItem('notifshown','true');
+    }
+  });
+
+  const onLogout = async () => {
+    const response = await axios.get("/api/users/logout");
+    console.log(response);
+    setLoginSuccess("false");
+    localStorage.clear();
+    router.push("/login");
+  };
+  const getUserDetails = async () => {
+    if (data.length > 0) {
+      setData("");
+      return;
+    }
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+  return (
+    <div className="flex flex-col text-center justify-center min-h-screen text-red-600">
+      <Toaster position="top-center" />
+      <div>{data}</div>
+      <div> toh kaise hai aap log</div>
+      <button onClick={onLogout} className="">
+        logout
+      </button>
+      <button className="text-red" onClick={getUserDetails}>
+        {data.length == 0 ? "GET DETAILS" : "HIDE DETAILS"}
+      </button>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
